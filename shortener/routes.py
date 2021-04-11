f2f0458f52df1d97caa358181f25d41a6c57505c
vandit <vasavandit@gmail.com>
2021-04-11 21:47:04 +0530

    adding all url table

--- shortener/routes.py
+++ shortener/routes.py
@@ -11,25 +11,14 @@ base_url = "http://127.0.0.1:5000/"
 def index():
     if request.method == "POST":
         url_received = request.form["entered_url"]
-        url_check = db.collection('urls').where("long_url", "==", url_received).get()   # check if url already exists
-        for url in url_check:
-            exist_url = url.to_dict()
-            res = bool(exist_url)
-            if res:
-                return render_template('index.html', short_url=exist_url['short_url'])
-            else:
-                short = shorten_url()
-                display_short = base_url + short
-                data = {
-                    'long_url': url_received,
-                    'short': short,
-                    'short_url': display_short
-                }
-                db.collection('urls').document(short).set(data)
-                if short is not None:
-                    return render_template('index.html', short_url=display_short)
+        short = shorten_url()
+        display_short = base_url + short
+        database(url_received, short, display_short)
+        docs = db.collection('urls').get()
+        return render_template('index.html', short_url=display_short, docs=docs)
     else:
-        return render_template('index.html', short_url="")
+        docs = db.collection('urls').get()
+        return render_template('index.html', short_url="", docs=docs)
 
 
 @app.route("/<short>")
@@ -42,15 +31,6 @@ def redirecting(short):
         return "<h1>URL Doesn't exists</h1>"
 
 
-@app.route('/all-urls')
-def display_all():
-    docs = db.collection('urls').get()
-    for doc in docs:
-        print(doc.to_dict())
-        arr = []
-    return "hello"
-
-
 def shorten_url():
     letters = string.ascii_lowercase + string.ascii_uppercase
     while True:
@@ -59,3 +39,12 @@ def shorten_url():
         collection = db.collection('urls').where("short", "==", rand_letters).get()
         if not collection:
             return rand_letters
+
+
+def database(url_received, short, display_short):
+    data = {
+        'long_url': url_received,
+        'short': short,
+        'short_url': display_short
+    }
+    db.collection('urls').document(short).set(data)
